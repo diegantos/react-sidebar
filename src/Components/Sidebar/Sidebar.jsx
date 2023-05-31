78caffd01ee3d91bd168dcfa425d7451abbe8aeb
Diegantos <diegantos8@gmail.com>
2023-05-31 18:26:00 +0200

    new styles

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -7,27 +7,39 @@ export const Sidebar = () => {
     { id: 2, name: "Create", icon: "add_box" },
     { id: 3, name: "Profile", icon: "person" },
     { id: 4, name: "Favourites", icon: "favorite" },
-    { id: 5, name: "Search", icon: "search" },
+    { id: 5, name: "Search", icon: "search" }
   ]
-  const menuHeader = ['menu','signup','settings']
+  const menuHeader = [
+    { id: 0, name: 'menu', icon:'menu'},
+    { id: 1, name: 'signup', icon:'lock'},
+    { id: 2, name: 'settings', icon:'settings'}
+  ]
 
   return (
     <>
-      <div className="Sidebar">
-        <header className="Sidebar-header">
-            
+      <aside className="Sidebar">
+        <header className="Header">
+          <ul className="Header-ul">
+            {menuHeader.map(({id, name, icon}) => (
+              <li key={id} className="Header-li">
+                <button className="Header-button">
+                  <span className="material-symbols-outlined">{icon}</span>
+                </button>
+              </li>
+            ))}
+          </ul>
         </header>
         <ul className="Sidebar-ul">
           {menuItems.map(({ id, name, icon }) => (
             <li key={id} className="Sidebar-li">
               <button className="Sidebar-button">
                 <span className="material-symbols-outlined">{icon}</span>
-                <span>{name}</span>
+                <span className="Sidebar-span">{name}</span>
               </button>
             </li>
           ))}
         </ul>
-      </div>
+      </aside>
     </>
   )
 }
