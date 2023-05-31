c7147cee7a3d8ed209ae871305f09bf7bb660634
Diego Santos <diegantos8@gmail.com>
2023-05-31 11:47:12 +0200

    adding styles

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,32 @@
 import "./Sidebar.scss";
 
 export const Sidebar = () => {
+  const menuItems = [
+    { id: 0, name: "Home", icon: "home" },
+    { id: 1, name: "Apss", icon: "apps" },
+    { id: 2, name: "Create", icon: "add_box" },
+    { id: 3, name: "Profile", icon: "person" },
+    { id: 4, name: "Favourites", icon: "favorite" },
+    { id: 5, name: "Search", icon: "search" },
+  ]
+  const menuHeader = []
   return (
-   <>
-    <div>Hello world!</div>
-    <span className="material-symbols-outlined">home</span>
-    <span className="material-symbols-rounded">face</span>
-    <span className="material-symbols-sharp">face</span>
-   </> 
+    <>
+      <div className="Sidebar">
+        <header className="Sidebar-header">
+
+        </header>
+        <ul className="Sidebar-ul">
+          {menuItems.map(({ id, name, icon }) => (
+            <li key={id} className="Sidebar-li">
+              <button className="Sidebar-button">
+                <span className="material-symbols-outlined">{icon}</span>
+                <span>{name}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
