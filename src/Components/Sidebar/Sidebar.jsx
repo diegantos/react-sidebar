26b9d9a92f25122204da1a5ea37564e462dec468
Diego Santos <diegantos8@gmail.com>
2023-05-31 09:52:09 +0200

    install and import material-symbols

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,6 +4,9 @@ export const Sidebar = () => {
   return (
    <>
     <div>Hello world!</div>
+    <span className="material-symbols-outlined">home</span>
+    <span className="material-symbols-rounded">face</span>
+    <span className="material-symbols-sharp">face</span>
    </> 
   )
 }
