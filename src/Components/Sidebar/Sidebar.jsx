e7f3c3cbaac753e97a4378221dbd26f6287b29fd
Diego Santos <diegantos8@gmail.com>
2023-06-01 08:54:03 +0200

    Login

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -14,13 +14,18 @@ export const Sidebar = () => {
     { id: 1, name: 'signup', icon:'lock'},
     { id: 2, name: 'settings', icon:'settings'}
   ]
+  const userLogin = [
+    { id: 0, icon: 'account_circle', type: 'text', placeholder: 'Username'},
+    { id: 1, icon: 'password', type: 'password', placeholder: 'Password'},
+    { id: 2, icon: 'email', type: 'email', placeholder: 'Email'}
+  ]
 
   return (
     <>
       <aside className="Sidebar">
         <header className="Header">
           <ul className="Header-ul">
-            {menuHeader.map(({id, name, icon}) => (
+            {menuHeader.map(({id, icon}) => (
               <li key={id} className="Header-li">
                 <button className="Header-button">
                   <span className="material-symbols-outlined">{icon}</span>
@@ -29,6 +34,7 @@ export const Sidebar = () => {
             ))}
           </ul>
         </header>
+
         <ul className="Sidebar-ul">
           {menuItems.map(({ id, name, icon }) => (
             <li key={id} className="Sidebar-li">
@@ -36,6 +42,17 @@ export const Sidebar = () => {
                 <span className="material-symbols-outlined">{icon}</span>
                 <span className="Sidebar-span">{name}</span>
               </button>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="Sesion">
+          {userLogin.map(({id, icon, type, placeholder})=>(
+            <li key={id} className="Sesion-li">
+              <span className="Sesion-icon material-symbols-outlined">{icon}</span>
+              <input 
+                type={type}
+                placeholder={placeholder} />
             </li>
           ))}
         </ul>
